38dd80cef2438c7a456aa852516370b9bf0b8e23
Andrzej Prochyra <andrzej.prochyra@digital.cabinet-office.gov.uk>
2026-09-23 12:28:58 +0100

    PP-15896: copy logo PNGs to dist directory and serve from there
    - Add PNG files to `build:copy` script
    - Update static file configuration to serve from new `assets/logos` directory

--- src/web/server.js
+++ src/web/server.js
@@ -89,7 +89,7 @@ function configureServingPublicStaticFiles(instance) {
   instance.use('/assets/fonts', express.static(path.join(process.cwd(), 'node_modules/govuk-frontend/dist/govuk/assets/fonts'), cache))
   instance.use('/images/favicon.ico', express.static(path.join(process.cwd(), 'node_modules/govuk-frontend/dist/govuk/assets/images/', 'favicon.ico'), cache))
   instance.use('/javascripts/govuk-frontend.js', express.static(path.join(process.cwd(), 'node_modules/govuk-frontend/dist/govuk/all.bundle.js'), cache))
-  instance.use('/assets/logos', express.static(path.join(process.cwd(), 'src/assets/logos'), cache))
+  instance.use('/assets/logos', express.static(path.join(__dirname, '../assets/logos'), cache))
 }
 
 function configureClientSessions(instance) {
